cc17275b0a20ab5048e0a4e96de5a854fd80f558
ingaSiu <ingasiud@gmail.com>
2023-03-22 09:44:58 +0200

    children as a function example

---
+++ src/examples/childrenAsFunc.tsx
@@ -0,0 +1,19 @@
+// Render props => kai daliname koda tarp react components, naudodami prop, kurio verte yra funkcija
+
+// formiko naudoja children as a function
+
+const Examples = () => {
+  return (
+    <>
+      {/* <Formik>
+        {({ value, handleSubmit, handleReset }) => (
+          <Form>
+            <p>hi there</p>
+          </Form>
+        )}
+      </Formik> */}
+    </>
+  );
+};
+
+export default Examples;
