4059d4d21031801662c432d4a173ded456498dff
ingaSiu <ingasiud@gmail.com>
2023-03-22 09:44:29 +0200

    added pages

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Examples from './examples/ExamplesTable';
+import RefExample from './examples/refsExample';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <RefExample />
+      <Examples />
+    </div>
+  );
+};
+
+export default App;
+
