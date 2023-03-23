8ef552e55fc925f91797d226880c7a9c1d499e13
ingaSiu <ingasiud@gmail.com>
2023-03-23 11:57:17 +0200

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import Example from './examples/MemoAndCallbackExample';
 import Examples from './examples/ExamplesTable';
+import HookExample from './examples/hooks/customHookExample';
 import RefExample from './examples/refsExample';
+import ToggleExample from './examples/ToggleExample';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      {/* <h1>Hello</h1>
       <RefExample />
-      <Examples />
+      <Examples /> */}
+      <HookExample />
+      {/* <ToggleExample />
+      <Example /> */}
     </div>
   );
 };
